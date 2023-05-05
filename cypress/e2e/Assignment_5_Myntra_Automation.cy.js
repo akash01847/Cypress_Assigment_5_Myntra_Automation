db2f89a9a2eeb8414142489ba07e1beb8abfee28
akash01847 <124980188+akash01847@users.noreply.github.com>
2023-05-05 16:40:18 +0530

    Update Assignment_5_Myntra_Automation.cy.js

--- cypress/e2e/Assignment_5_Myntra_Automation.cy.js
+++ cypress/e2e/Assignment_5_Myntra_Automation.cy.js
@@ -13,22 +13,20 @@ describe('Search Product on Myntra', () => {
       .find('a')
       .invoke('removeAttr', 'target')
       .eq(0).click();
-
+      //.size-buttons-size-button.size-buttons-size-button-default
+      // div.size-buttons-buttonContainer p.size-buttons-unified-size
     cy.get('.size-buttons-size-button.size-buttons-size-button-default').eq(1).click({ force: true })
 
-    cy.wait(2000);
-    cy.get('div.pdp-add-to-bag.pdp-button.pdp-flex.pdp-center').click({ force: true });
+
+    cy.get('div.pdp-add-to-bag.pdp-button.pdp-flex.pdp-center').click({ force: true })
 
     // Click on the bag icon to view the cart
-    cy.get('.myntraweb-sprite.desktop-iconBag.sprites-headerBag').click();
+    cy.get('.myntraweb-sprite.desktop-iconBag.sprites-headerBag').click()
 
     cy.reload()
-    cy.get('button.css-etguer').should('exist').click({ force: true });
+    cy.get('button.css-etguer').should('exist').click({ force: true })
 
     // Check that the URL contains the cart page
-    cy.url().should('contain', '/checkout/cart');
+    cy.url().should('contain', '/checkout/cart')
   })
 })
-
-
-
