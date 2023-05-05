beb979e3f3f9344ee3e7667da05d57284c8944a6
akash01847 <124980188+akash01847@users.noreply.github.com>
2023-05-05 18:00:24 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  plugins: ["cypress-mochawesome-reporter"],
+
+  reporter: "cypress-multi-reporters",
+
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      quiet: true,
+      overwrite: true,
+      html: true,
+      json: true,
+    },
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-  reporter: "cypress-multi-reporters",
-    reporterOptions: {
-        reporterEnabled: "mochawesome",
-        mochawesomeReporterOptions: {
-            reportDir: "cypress/reports/mocha",
-            quite: true,
-            overwrite: true,
-            html: true,
-            json: true
-  }
-}
 });
